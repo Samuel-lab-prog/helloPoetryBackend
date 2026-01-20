2f310928ed37d0de9a557a0b584c636444c983a2
Samuel <sg1976387@gmail.com>
2026-01-20 19:17:32 -0300

    refactor

--- src/domains/hey/adapters/http/queries/Services.ts
+++
@@ -1,9 +0,0 @@
-import { queriesRepository } from '../../../infra/queries-repository/Repository';
-
-export interface QueriesServices {
-  // methods will be added here
-}
-
-export const queriesServices: QueriesServices = {
-  // factory bindings will be added here
-};
