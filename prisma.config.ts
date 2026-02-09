586f5ea45ee928151afcb01f2c51db3b5808d67e
Samuel <sg1976387@gmail.com>
2026-02-09 07:51:27 -0300

    workflow: added tests

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,15 +2,10 @@ import 'dotenv/config';
 import { defineConfig } from 'prisma/config';
 
 let url: string;
-if (process.env.NODE_ENV === 'test') {
-	url = process.env.TEST_DB_URL!;
-} else if (process.env.NODE_ENV === 'dev') {
-	url = process.env.DEV_DB_URL!;
-} else if (process.env.NODE_ENV === 'prod') {
-	url = process.env.PROD_DB_URL!;
-} else {
-	throw new Error('NODE_ENV is not set to a valid value');
-}
+if (process.env.NODE_ENV === 'test') url = process.env.TEST_DB_URL!;
+else if (process.env.NODE_ENV === 'development') url = process.env.DEV_DB_URL!;
+else if (process.env.NODE_ENV === 'production') url = process.env.PROD_DB_URL!;
+else url = process.env.TEST_DB_URL!;
 
 export default defineConfig({
 	schema: 'src/generic-subdomains/persistance/prisma/schema.prisma',
--- src/generic-subdomains/persistance/prisma/PrismaClient.ts
+++ src/generic-subdomains/persistance/prisma/PrismaClient.ts
@@ -3,15 +3,12 @@ import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from './generated/client';
 
 let url: string;
-if (process.env.NODE_ENV === 'prod') {
-	url = `${process.env.PROD_DB_URL}`;
-} else if (process.env.NODE_ENV === 'dev') {
+if (process.env.NODE_ENV === 'production') url = `${process.env.PROD_DB_URL}`;
+else if (process.env.NODE_ENV === 'development')
 	url = `${process.env.DEV_DB_URL}`;
-} else if (process.env.NODE_ENV === 'test') {
-	url = `${process.env.TEST_DB_URL}`;
-} else {
-	throw new Error('NODE_ENV is not set to a valid value');
-}
+else if (process.env.NODE_ENV === 'test') url = `${process.env.TEST_DB_URL}`;
+else url = `${process.env.TEST_DB_URL}`;
+
 const connectionString = url;
 
 const adapter = new PrismaPg({ connectionString });
