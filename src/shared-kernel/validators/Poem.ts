d9553f770c354175eab1a776058c27333e30e2e7
Samuel <sg1976387@gmail.com>
2026-02-13 13:52:44 -0300

    REFACTOR

--- src/shared-kernel/validators/Poem.ts
+++ src/shared-kernel/validators/Poem.ts
@@ -8,42 +8,38 @@ import type {
 
 export function poem(poem: PoemBasicInfo) {
 	if (!poem.exists)
-		throw new NotFoundError(`Poem with id "${poem.id}" not found`);
+		throw new NotFoundError(`Poem with id ${poem.id} not found`);
 
 	return {
 		withVisibility(allowedVisibilities: PoemVisibility[]) {
-			if (!allowedVisibilities.includes(poem.visibility)) {
+			if (!allowedVisibilities.includes(poem.visibility)) 
 				throw new ForbiddenError(
-					`Cannot perform this action on a poem with visibility "${poem.visibility}"`,
+					`Cannot perform this action on a poem with visibility ${poem.visibility}`,
 				);
-			}
 			return this;
 		},
 
 		withModerationStatus(allowedModerationStatuses: PoemModerationStatus[]) {
-			if (!allowedModerationStatuses.includes(poem.moderationStatus)) {
+			if (!allowedModerationStatuses.includes(poem.moderationStatus)) 
 				throw new ForbiddenError(
-					`Cannot perform this action on a poem with moderation status "${poem.moderationStatus}"`,
+					`Cannot perform this action on a poem with moderation status ${poem.moderationStatus}`,
 				);
-			}
 			return this;
 		},
 
 		withCommentability(allowedCommentability: boolean) {
-			if (poem.isCommentable !== allowedCommentability) {
+			if (poem.isCommentable !== allowedCommentability) 
 				throw new ForbiddenError(
-					`Cannot perform this action on a poem with commentability "${poem.isCommentable}"`,
+					`Cannot perform this action on a poem with commentability ${poem.isCommentable}`,
 				);
-			}
 			return this;
 		},
 
 		withStatus(allowedStatuses: PoemStatus[]) {
-			if (!allowedStatuses.includes(poem.status)) {
+			if (!allowedStatuses.includes(poem.status)) 
 				throw new ForbiddenError(
-					`Cannot perform this action on a poem with status "${poem.status}"`,
+					`Cannot perform this action on a poem with status ${poem.status}`,
 				);
-			}
 			return this;
 		},
 	};
--- src/shared-kernel/validators/User.ts
+++ src/shared-kernel/validators/User.ts
@@ -4,22 +4,20 @@ import { ForbiddenError, NotFoundError } from '@DomainError';
 
 export function user(user: UserBasicInfo) {
 	if (!user.exists)
-		throw new NotFoundError(`User with id "${user.id}" not found`);
+		throw new NotFoundError(`User with id ${user.id} not found`);
 	return {
 		withStatus(allowedStatuses: UserStatus[]) {
-			if (!allowedStatuses.includes(user.status)) {
+			if (!allowedStatuses.includes(user.status)) 
 				throw new ForbiddenError(
-					`User with status "${user.status}" cannot perform this action`,
+					`User with status ${user.status} cannot perform this action`,
 				);
-			}
 			return this;
 		},
 		withRole(allowedRoles: UserRole[]) {
-			if (!allowedRoles.includes(user.role)) {
+			if (!allowedRoles.includes(user.role)) 
 				throw new ForbiddenError(
-					`User with role "${user.role}" cannot perform this action`,
+					`User with role ${user.role} cannot perform this action`,
 				);
-			}
 			return this;
 		},
 	};
